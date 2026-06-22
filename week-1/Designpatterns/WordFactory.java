e214a7e3f0ddc940e6bf9cc82754c49a1492a55d
Chandaka Venkata Divya Harshitha <harshithachandaka14@gmail.com>
2026-06-22 22:26:48 +0530

    Add files via upload

---
+++ week-1/Designpatterns/WordFactory.java
@@ -0,0 +1,8 @@
+package FactoryMethodPattern;
+public class WordFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
